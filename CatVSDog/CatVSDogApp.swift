c77b0dbe46f4b8c5335dd4f782793e080e1efe99
Carson Gross <carsongro@gmail.com>
2023-12-03 20:33:20 -0700

    Initial Commit

---
+++ CatVSDog/CatVSDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatVSDogApp.swift
+//  CatVSDog
+//
+//  Created by Carson Gross on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CatVSDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
